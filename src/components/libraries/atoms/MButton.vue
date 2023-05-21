5fb6a7d8e818387c9999c58633540793592cc360
liva <ebammo20@gmail.com>
2023-05-21 22:57:04 +0330

    Add button

---
+++ src/components/libraries/atoms/MButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <button class="m-button" v-bind="$attrs">
+    <slot />
+  </button>
+</template>
+
+<style>
+.m-button {
+  background-color: #40b983; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 1rem;
+}
+</style>
