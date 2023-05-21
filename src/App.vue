385da2a9085de17cdde433fe39bcb2e7524e9088
liva <ebammo20@gmail.com>
2023-05-21 23:36:00 +0330

    Use MInput

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,18 @@
 <script setup>
+import MButton from './components/libraries/atoms/MButton.vue';
+import MInput from './components/libraries/atoms/MInput.vue';
+import {ref} from "vue";
+let mv = ref()
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-    </div>
-  </header>
-
   <main>
+    <MButton>{{mv}}</MButton>
+    <MInput v-model="mv"/>
+
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
